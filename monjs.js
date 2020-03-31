2439af04bac6b67457e0d5fbee49a4bffd48b44f
chloesln <60424292+chloesln@users.noreply.github.com>
2020-03-31 20:27:29 +0200

    1

--- monjs.js
+++ monjs.js
@@ -1,1 +1,12 @@
-alert('Chloe');
+/*var start = 'Bonjour ', name, end = ' !', result;
+
+name   = prompt('Quel est votre prénom ?');
+result = start + name + end;
+alert(result);*/
+
+function chloe(arg){
+    alert('mon prenom est : ' + arg);
+
+}
+
+chloe('et voila');
